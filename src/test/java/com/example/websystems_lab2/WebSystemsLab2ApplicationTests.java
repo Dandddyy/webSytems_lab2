1d3342649005d92d2f274865114c05049d47c6cd
Dandddyy <kovalenko.danyl@gmail.com>
2024-10-10 16:22:37 +0300

    Added unit test

--- src/test/java/com/example/websystems_lab2/WebSystemsLab2ApplicationTests.java
+++ src/test/java/com/example/websystems_lab2/WebSystemsLab2ApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.websystems_lab2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class WebSystemsLab2ApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testProductCreation() {
+        Product product = new Product("1", "Test Product");
+        assertEquals("1", product.getId());
+        assertEquals("Test Product", product.getName());
     }
 
 }
